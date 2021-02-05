8b222b489793ce40709615e48d8adad9a51c9c7b
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-06 03:08:53 +0530

    :tada: 4

---
+++ Assignment-1/4.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("The address of main() is %p\n", main);
+    printf("The address of main() is %p\n", &main);
+    printf("The address of printf() is %p\n", printf);
+    printf("The address of printf() is %p\n", &printf);
+
+    return 0;
+}
