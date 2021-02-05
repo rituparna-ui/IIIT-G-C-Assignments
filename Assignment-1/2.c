714aa3823ab4388755b7fd3919aaf2d7b5f46af1
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-06 03:05:12 +0530

    :tada: 2

---
+++ Assignment-1/2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main()
+{
+    int my_first_variable;
+    int mySecondVariable;
+    int MyThirdVariable;
+    // int char;
+    int n;
+    int number;
+    int _;
+    int _number_;
+    // int 2months;
+    int months2;
+    int months_2;
+    // int months 2;
+    // int months two
+    // int months?;
+
+    return 0;
+}
