e45f0762b1977763c006732216364b740b35a9d6
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-06 14:16:53 +0530

    :tada: 3

---
+++ Assignment-2/2.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float a = 5,
+          b = 4,
+          c = 3,
+          d = 2,
+          e = 1;
+
+    float avg = (a + b + c + d + e) / 5;
+
+    printf("%f", avg);
+
+    return 0;
+}
---
+++ Assignment-2/3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    float radius = 0, area = 0;
+    radius = 7;
+    area = (22 / 7) * radius * radius;
+    printf("%g", area);
+
+    return 0;
+}
