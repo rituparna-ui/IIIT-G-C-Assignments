cfc2023f04f57b3e0f628efa1c49442eca4c0d3e
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-07 00:58:24 +0530

    :partying_face: 21

---
+++ Assignment-2/21.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main()
+{
+    int a = 10, b = 15;
+    printf("a=%d b=%d", a, b);
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    printf("a=%d b=%d", a, b);
+
+    return 0;
+}
