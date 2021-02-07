28b45240ca303b447b4b441d45f23409ffafeb1f
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-07 16:06:21 +0530

    :partying_face: 12

---
+++ Assignment-3/12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+    float a, b, c;
+    if (a + b + c == 180)
+    {
+        printf("Valid");
+    }
+    else
+    {
+        printf("Invalid");
+    }
+
+    return 0;
+}
