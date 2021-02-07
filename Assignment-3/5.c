5567c7588041030d99ff5b41f83efbb07fcde257
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-07 15:41:12 +0530

    :partying_face: 3

---
+++ Assignment-3/5.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int num;
+    if (num > 0)
+    {
+        printf("Positive");
+    }
+    else if (num < 0)
+    {
+        printf("negative");
+    }
+    else
+    {
+        printf("Zero");
+    }
+
+    return 0;
+}
