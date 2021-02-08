30edc0a49f06c5c72f1ed4ab9074d500a0a2c961
Rituparna Warwatkar <rwarwatkar@gmail.com>
2021-02-08 13:25:13 +0530

    :partying_face: 3

--- Assignment-7/3.c
+++ Assignment-7/3.c
@@ -2,6 +2,17 @@
 
 int main()
 {
-    
+    int myArr[5];
+    for (int i = 0; i < 5; i++)
+    {
+        printf("Enter element at position %d\n", i);
+
+        scanf("%d", &myArr[i]);
+    }
+    for (int i = 0; i < 5; i++)
+    {
+        printf("%d\n", myArr[i]);
+    }
+
     return 0;
 }
